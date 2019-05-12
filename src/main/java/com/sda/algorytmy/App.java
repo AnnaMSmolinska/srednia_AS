2e6cf0ac2f635715fe974d371b0620b9698dcb99
Anna Smolinska <anias05@tlen.pl>
2019-05-12 15:32:21 +0100

    srednia ver2

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,13 +1,21 @@
 package com.sda.algorytmy;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int wynik = 0;
+        for (int i = 0; i < n; i++) {
+            int a = scanner.nextInt();
+            wynik = wynik + a;
+
+        }
+        System.out.println(wynik);
     }
+
 }
